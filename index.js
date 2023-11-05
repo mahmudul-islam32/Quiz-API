c0776373230656abbef83f0a960de802607b0371
Mahmudul Islam <mahmud@iMac-von-Mahmudul.local>
2023-11-05 08:46:18 +0100

    Add quiz Api

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const port = 3000; // You can use any port you prefer
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Load your JSON data from the file
+const jsonData = JSON.parse(fs.readFileSync('questions.json', 'utf8'));
+
+// Define a route to get all questions
+app.get('/questions', (req, res) => {
+  res.json(jsonData);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
